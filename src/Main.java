c659ed3e66342372bdb0ac70947d6d30f764e44b
NguyenHongQuan <nhquan2622003@gmail.com>
2025-03-06 08:18:23 +0700

    minh chung nodesign va state

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 //        No design pattern
         Payment p = new Payment("123", "Bank Transfer");
-
+        System.out.println("=====================================");
 //        State pattern
         PaymentContext pc = new PaymentContext();
         pc.setState(new COD());
